11b8b596e5615d4673916b98f8d25e22b021a854
Brendan Bard <bbard@purdue.edu>
2017-06-27 09:58:35 -0400

    improved Day 1 Homework

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ function updateDiv(ev){
 ev.preventDefault()
 const y = ev.target
 const color = y.personColor
+const name = y.personName
 const div = document.querySelector('div')
-const details = document.querySelector('#details')
-div.textContent = color.value
+const details = document.querySelector('#stats p')
+div.textContent = name.value + " likes: " + color.value 
 details.textContent = color.value
-div.style.backgroundColor = color.value
-div.style.outlineColor = color.value
 div.style.color = color.value
 }
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', updateHeading)
+//personForm.addEventListener('submit', updateHeading)
 personForm.addEventListener('submit', updateDiv)

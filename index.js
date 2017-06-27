561c2d2a710dc2f42ca1a36587f40e3e0c23d4d2
Brendan Bard <bbard@purdue.edu>
2017-06-27 14:38:23 -0400

    finished day 2 hmwrk

--- index.js
+++ index.js
@@ -6,18 +6,7 @@ function handleSubmit(ev) {
     const age = f.age.value
     const favoriteColor = f.favoriteColor.value
 
-    const list = document.createElement('ul')
-    const nameItem = renderListItem(`Name: ${name}`)
-    const ageItem = renderListItem(`Age: ${age}`)
-    const favoriteColorItem = renderListItem('Favorite Color: ')
-    list.appendChild(nameItem)
-    list.appendChild(ageItem)
-    list.appendChild(favoriteColorItem)
-
-
-
-    const div = renderColor(favoriteColor)
-    list.appendChild(div)
+    const list = renderList(name, age, favoriteColor)
     const stats = document.querySelector('#stats')
     stats.appendChild(list)
 }
@@ -30,11 +19,25 @@ function renderColor(favoriteColor) {
     colorDiv.style.height = '3rem'
     return colorDiv
 }
+
 //renderListItem
 function renderListItem(contentType) {
     const content = document.createElement('li')
     content.textContent = contentType
     return content
+}
+//renderList
+function renderList(name, age, favoriteColor) {
+    const list = document.createElement('ul')
+    const nameItem = renderListItem(`Name: ${name}`)
+    const ageItem = renderListItem(`Age: ${age}`)
+    const favoriteColorItem = renderListItem('Favorite Color: ')
+    list.appendChild(nameItem)
+    list.appendChild(ageItem)
+    list.appendChild(favoriteColorItem)
+    const div = renderColor(favoriteColor)
+    list.appendChild(div)
+    return list
 }
 
 const personForm = document.querySelector('#person-form')

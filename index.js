b8a6d998df9f6c5e9a9c9ad9ce38be28ee19a298
Brendan Bard <bbard@purdue.edu>
2017-06-27 11:38:25 -0400

    add stats in an unordered list

--- index.js
+++ index.js
@@ -13,9 +13,25 @@ const color = y.personColor
 const name = y.personName
 const div = document.querySelector('div')
 const details = document.querySelector('#stats p')
-div.textContent = name.value + " likes: " + color.value 
-details.textContent = color.value
-div.style.color = color.value
+
+const list = document.createElement('ul')
+const nameItem = document.createElement('li')
+nameItem.textContent = `Name: ${name.value} `
+list.appendChild(nameItem)
+
+
+//const ageItem = document.createElement('li')  etc
+const colorItem = document.createElement('li')
+colorItem.textContent = `Color: ${color.value}`
+colorItem.style.backgroundColor = color.value
+list.appendChild(colorItem)
+
+
+stats.appendChild(list)
+
+//div.textContent = name.value + " likes: " + color.value 
+//details.textContent = color.value
+//div.style.color = color.value
 }
 
 const personForm = document.querySelector('#person-form')
